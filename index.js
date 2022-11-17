2099794cc50bc478d2921ea80d17bc415ea3f94a
Chris Eun <eunchristopher@gmail.com>
2022-11-16 20:08:55 -0800

    scoping example using JS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function newImage(url, left, bottom){
+function newImage(url, left, bottom) {
     let object = document.createElement('img')
     object.src = url
     object.style.position = 'fixed'
@@ -8,11 +8,9 @@ function newImage(url, left, bottom){
     return object
 }
 
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
-}
 
-function newInventory(){
+
+function newInventory() {
     let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
@@ -26,6 +24,22 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+
+    function newItem(url, left, bottom) {
+        let item = newImage(url, left, bottom)
+        item.addEventListener('click', function () {
+            item.remove()
+            let inventoryItem = document.createElement('img')
+            inventoryItem.src = url
+            inventory.append(inventoryItem)
+        })
+    }
+
+   newItem('assets/sword.png', 500, 555)
+newItem('assets/shield.png', 165, 335)
+newItem('assets/staff.png', 600, 250)
+
+
 }
 
 newInventory()
@@ -35,7 +49,3 @@ newImage('assets/pillar.png', 350, 250)
 newImage('assets/pine-tree.png', 450, 350)
 newImage('assets/crate.png', 150, 350)
 newImage('assets/well.png', 500, 575)
-
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
